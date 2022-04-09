88ebf661b41457bee2f7b66fe1dec0eb65b0cd7f
LeonardoMessaWagner <leonardowagner22@outlook.com>
2022-04-09 14:19:14 -0300

    ajustes classes filhas Pessoa

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,30 +1,39 @@
 public class Usuario extends Pessoa {
 
-    public Usuario(String nome, String email, String cpf, String login, String senha, Pessoa.Nivel nivel) {
-        super(nome, email, cpf, login, senha, nivel);
-        
-    }
     String cartao;
     String endereco;
     String[] ListaSenhas;
+
+    public Usuario(String nome, String email, String cpf, String login, String senha, Pessoa.Nivel nivel, String cartao,
+            String endereco, String[] listaSenhas) {
+        super(nome, email, cpf, login, senha, nivel);
+        this.cartao = cartao;
+        this.endereco = endereco;
+        ListaSenhas = listaSenhas;
+    }
+
     public String getCartao() {
         return cartao;
     }
+
     public void setCartao(String cartao) {
         this.cartao = cartao;
     }
+
     public String getEndereco() {
         return endereco;
     }
+
     public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
+
     public String[] getListaSenhas() {
         return ListaSenhas;
     }
+
     public void setListaSenhas(String[] listaSenhas) {
         ListaSenhas = listaSenhas;
     }
 
-    
 }
