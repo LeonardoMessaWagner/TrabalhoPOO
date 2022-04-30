bada27149b40706aebf322c8a717bfaed00647fe
Zoty <34866157+ZotyDev@users.noreply.github.com>
2022-04-29 21:48:28 -0300

    Modified

--- src/Senha.java
+++ src/Senha.java
@@ -1,58 +1,27 @@
-public class Senha {
-
-    String descricao;
-    String login;
-    String senha;
+public abstract class Senha {
+    protected String descricao;
+    protected String login;
+    protected String senha;
     
-    
-
+    // Construtor Senha
     public Senha(String descricao, String login, String senha) {
         this.descricao = descricao;
         this.login = login;
         this.senha = senha;
     }
 
+    // Método abstrato aplicar
+    abstract public void aplicar();
 
+    // Descricao get/set
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+    
+    // Login get/set
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
 
-    public void Aplicar() {
-
-    }
-
-
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-
-
-    public String getLogin() {
-        return login;
-    }
-
-
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-
-
-    public String getSenha() {
-        return senha;
-    }
-
-
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-   
+    // Senha get/set
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,39 +1,27 @@
-public class Usuario extends Pessoa {
+import java.util.ArrayList;
 
+public class Usuario extends Pessoa {
     String cartao;
     String endereco;
-    String[] ListaSenhas;
+    ArrayList<Senha> ListaSenhas;
 
-    public Usuario(String nome, String email, String cpf, String login, String senha, Pessoa.Nivel nivel, String cartao,
-            String endereco, String[] listaSenhas) {
+    // Construtor Usuario
+    public Usuario(String nome, String email, String cpf, String login, String senha, Pessoa.Nivel nivel, String cartao, String endereco, ArrayList<Senha> listaSenhas) {
         super(nome, email, cpf, login, senha, nivel);
         this.cartao = cartao;
         this.endereco = endereco;
         ListaSenhas = listaSenhas;
     }
 
-    public String getCartao() {
-        return cartao;
-    }
+    // Cartão get/set
+    public String getCartao() { return cartao; }
+    public void setCartao(String cartao) { this.cartao = cartao; }
 
-    public void setCartao(String cartao) {
-        this.cartao = cartao;
-    }
+    // Endereço get/set
+    public String getEndereco() { return endereco; }
+    public void setEndereco(String endereco) { this.endereco = endereco; }
 
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String[] getListaSenhas() {
-        return ListaSenhas;
-    }
-
-    public void setListaSenhas(String[] listaSenhas) {
-        ListaSenhas = listaSenhas;
-    }
-
+    // ListaSenhas get/set
+    public ArrayList<Senha> getListaSenhas() { return ListaSenhas; }
+    public void setListaSenhas(ArrayList<Senha> listaSenhas) { ListaSenhas = listaSenhas; }
 }
