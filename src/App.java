55f1b8e1bee78af6b8f1a05b1212366f8a1b6f65
Zoty <34866157+ZotyDev@users.noreply.github.com>
2022-05-06 23:17:46 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,10 +1,7 @@
-
-
 public class App {
-    public static void main(String[] args) throws Exception {
-
-
-
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception 
+    {
+        Interface Tela = new Interface();
+        Tela.MostrarAções();
     }
 }
